6a21d57bddcc39f821be34e794c98813ab7fabbb
eman275 <eman.altweel11@gmail.com>
2023-01-23 21:11:42 +0200

    color picker

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import ColorPicker from "material-ui-color-picker";
+import ColorChip from "./ColorChip";
 
-function App() {
+const App = () => {
+  const [color, setColor] = React.useState("#000");
+  const handleOnChange = React.useCallback(color => {
+    console.log(color);
+    setColor(color);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ColorChip color={color}>Chip</ColorChip>
+      <ColorPicker
+        name="color"
+        defaultValue="#000"
+        // value={this.state.color} - for controlled component
+        onChange={handleOnChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/ColorChip.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import styled from "styled-components";
+
+type ColorRGBA = [number, number, number, number];
+
+interface ColorChipProps {
+  color?: string;
+  rgba?: ColorRGBA;
+}
+
+const getRGBA = (rgba: ColorRGBA) => ({
+  backgroundColor: `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3]})`
+});
+
+const StyledDiv = styled("div")(({ theme, ...props }) => {
+  const rgba = props["data-rgba"];
+  const color = props["data-color"];
+
+  let col;
+  if (rgba) {
+    col = `rgba(${rgba[0]}, ${rgba[1]}, ${rgba[2]}, ${rgba[3]})`;
+  }
+  if (color) {
+    col = `${props["data-color"]}`;
+  }
+
+  return {
+    backgroundColor: col
+  };
+});
+
+const ColorChip: React.FC<ColorChipProps> = ({
+  color,
+  rgba = [255, 0, 0, 1],
+  children,
+  ...props
+}) => (
+  <div>
+    <StyledDiv data-color={color} data-rgba={rgba}>
+      {JSON.stringify(color)}
+    </StyledDiv>
+    <p style={getRGBA(rgba)}>{JSON.stringify(rgba)}</p>
+  </div>
+);
+
+export default ColorChip;
